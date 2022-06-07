247ed2747a29cc2c4877591c598c701410b26672
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-06-06 18:57:45 -0600

    added a example image for graph showing visitor numbers

--- src/components/Visitors.js
+++ src/components/Visitors.js
@@ -1,10 +1,13 @@
 const Visitors = ({title, info}) => {
   return (
       <div className="visitors">
-          <h3>{title}</h3>
-          <h2>{info}</h2>
-          <div id="visitorsVisualization">
-              
+      <h3>{title}</h3>
+      <div className= "visits">
+        <h2>{info}</h2>
+        <p>Vistitors Today</p> 
+      </div>
+          <div id="visitorsVisual">
+              <img src="https://wpdatatables.com/wp-content/uploads/2020/12/Proof-of-concep-Chart.js-with-Background-Gradient.jpg"></img>
           </div>
     </div>
   )
